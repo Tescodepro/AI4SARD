fdd6c15429ba6430338b4819e3d55b3dc0e84833
Tescodepro <tescodepro@gmail.com>
2023-12-05 23:33:21 +0100

    add them

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'root', 'ai4sard0o2023', 'ai4sard');
+$connect = mysqli_connect('localhost', 'ai4sard', 'ai4sard0o2023', 'ai4sard');
 
 // Check connection
 if (!$connect) {
